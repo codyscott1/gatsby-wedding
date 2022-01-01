83c309ddaa84349ba807101a038861267948d076
Cody Crandall <cody@halfhelix.com>
2022-01-01 17:36:59 -0600

    fix several jsx-a11y recommendations

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -28,7 +28,7 @@ const GuestList = ({ guests, setGuests, confirmedReservationsCount }) => {
       </h4>
       <ul className="mb-3 list-group">
         {guests.map((guest, i) => (
-          <li
+          <checkbox
             className={`list-group-item d-flex justify-content-between lh-sm ${reservationCSS(
               guest
             )}`}
@@ -36,6 +36,7 @@ const GuestList = ({ guests, setGuests, confirmedReservationsCount }) => {
             onClick={() => {
               toggleGuestReservation(guest);
             }}
+            onKeyDown={() => toggleGuestReservation(guest)}
           >
             <h6 className="my-auto">{guest.name}</h6>
             <div className="form-check form-switch">
@@ -46,14 +47,14 @@ const GuestList = ({ guests, setGuests, confirmedReservationsCount }) => {
                 readOnly={true}
               />
             </div>
-          </li>
+          </checkbox>
         ))}
       </ul>
       <h4 className="mb-3 d-flex justify-content-between align-items-center ps-1 pe-4">
         <span>Confirmed reservations</span>
         <span
           className="badge rounded-pill"
-          style={{ backgroundColor: "#80977f" }}
+          style={{ backgroundColor: "#80977f", height: "30px", width: "30px" }}
         >
           {confirmedReservationsCount}
         </span>
